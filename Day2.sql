4696f03edcd6729a5cc0e6968f3c8f3738e3c272
Zainab <z.alkholaif@gmail.com>
2026-01-19 23:20:44 +0300

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -1,1 +1,24 @@
+-- Day 2: Identifying Incomplete Employee Records
 
+-- Question:
+-- Which employees have missing information across HR and payroll systems?
+
+-- Solution
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Salaries s
+    ON e.employee_id = s.employee_id
+WHERE s.employee_id IS NULL
+
+UNION 
+
+SELECT s.employee_id
+FROM Salaries s
+LEFT JOIN Employees e
+    ON s.employee_id = e.employee_id
+WHERE e.employee_id IS NULL
+
+ORDER BY employee_id ASC;
+
+-- Source:
+-- LeetCode 1965 — Employees With Missing Information
