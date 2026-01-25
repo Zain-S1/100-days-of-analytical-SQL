d2a6dde6e9cb8d69cc3774295a74094d8bc2a4a9
Zainab <z.alkholaif@gmail.com>
2026-01-25 15:45:46 +0300

    Update Day8.sql

--- Day8.sql
+++ Day8.sql
@@ -1,1 +1,13 @@
+-- Day 1: Filtering Products by Business Rules
 
+-- Question:
+-- Which products meet both health and sustainability criteria?
+
+-- Solution
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' 
+    AND recyclable = 'Y';
+
+-- Source:
+-- LeetCode 1757 — Recyclable and Low Fat Products
