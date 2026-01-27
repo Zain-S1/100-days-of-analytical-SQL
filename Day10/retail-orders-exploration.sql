32381dd5e74d6163f28d9e20b0d1c3e2532b0d1e
Zainab <z.alkholaif@gmail.com>
2026-01-27 22:52:30 +0300

    Update retail-orders-exploration.sql

--- Day10/retail-orders-exploration.sql
+++ Day10/retail-orders-exploration.sql
@@ -5,3 +5,17 @@
 -- Country, City, State, Postal Code, Region,
 -- Category, Sub Category, Product Id,
 -- cost price, List Price, Quantity, Discount Percent
+
+--------------------------------------------------
+-- 1. Total Revenue
+--------------------------------------------------
+SELECT
+    ROUND(
+        SUM(list_price * quantity * (1 - discount / 100.0)),
+        2
+    ) AS total_revenue
+FROM retail_orders
+
+-- Insight:
+-- Establishes top-line sales performance.
+
