13086d2b92bdf16a3297c681563f270b88a50a68
Zainab <z.alkholaif@gmail.com>
2026-02-07 16:29:44 +0300

    Update HackerRank-TheReport.sql

--- Day07/HackerRank-TheReport.sql
+++ Day07/HackerRank-TheReport.sql
@@ -1,13 +1,22 @@
--- Day 1: Filtering Products by Business Rules
+-- Day 7: JOINs with NULL Detection
 
 -- Question:
--- Which products meet both health and sustainability criteria?
+-- How can we associate students with grades while
+-- identifying cases where names should be hidden
+-- based on performance rules?
 
 -- Solution
-SELECT product_id
-FROM Products
-WHERE low_fats = 'Y' 
-    AND recyclable = 'Y';
+SELECT
+    CASE 
+        WHEN g.Grade < 8 THEN NULL
+        ELSE s.Name
+    END AS Name,
+    g.Grade,
+    s.Marks
+FROM Students s 
+JOIN Grades g 
+    ON s.Marks BETWEEN g.Min_Mark AND g.Max_Mark
+ORDER BY g.Grade DESC, s.Name ASC;
 
 -- Source:
--- LeetCode 1757 — Recyclable and Low Fat Products
+-- HackerRank — The Report
