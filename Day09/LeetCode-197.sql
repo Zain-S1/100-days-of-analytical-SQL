7cc80a25023a9aac80180da51ddaf4aace1e02f6
Zainab <z.alkholaif@gmail.com>
2026-02-09 15:28:49 +0300

    Update LeetCode-197.sql

--- Day09/LeetCode-197.sql
+++ Day09/LeetCode-197.sql
@@ -1,1 +1,14 @@
+-- Day 9: Time Awareness and Date Comparison
 
+-- Question:
+-- Which days recorded a higher temperature compared to the previous day?
+
+-- Solution
+SELECT d1.id
+FROM Weather d1
+JOIN Weather d2
+    ON d1.recordDate = DATE_ADD(d2.recordDate, INTERVAL 1 DAY)
+WHERE d1.temperature > d2.temperature;
+
+-- Source:
+-- LeetCode 197 — Rising Temperature
