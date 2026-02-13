976cf97b1a82599d78984aae2a2364574a983735
Zainab <z.alkholaif@gmail.com>
2026-02-13 17:52:18 +0300

    Update first-vs-returning-customers.sql

--- Day13/first-vs-returning-customers.sql
+++ Day13/first-vs-returning-customers.sql
@@ -10,19 +10,19 @@
 -- Solution
 SELECT
     o1.order_id,
-    r1.order_date,
-    r1.customer_id,
+    o1.order_date,
+    o1.customer_id,
     CASE
-        WHEN r1.order_date = (
-            SELECT MIN(r2.order_date)
-            FROM retail_orders r2
-            WHERE r2.customer_id = r1.customer_id;
+        WHEN o1.order_date = (
+            SELECT MIN(o2.order_date)
+            FROM orders o2
+            WHERE o2.customer_id = o1.customer_id;
         )
         THEN 'First-Time'
         ELSE 'returing'
     END AS customer_type
-FROM retail_orders r1
-ORDER BY r1.order_date;
+FROM orders o1
+ORDER BY o1.order_date;
 
 -- Source:
 -- Generic Orders Table (Concept Exercise)
