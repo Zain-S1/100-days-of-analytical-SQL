1a40f9cbeba72f4bdef13533b851b4bc39dd2664
Zainab <z.alkholaif@gmail.com>
2026-02-17 19:27:07 +0300

    Update running-total-revenue.sql

--- Day17/running-total-revenue.sql
+++ Day17/running-total-revenue.sql
@@ -1,1 +1,25 @@
+-- Day 16: Ranking Products by Profit
 
+-- Question:
+-- What is the profit ranking of each product based on total generated profit?
+
+-- Solution
+WITH product_profit AS (
+    SELECT
+        product_id,
+        SUM(
+            (list_price * quantity * (1 - discount / 100.0))
+            - (cost_price * quantity)
+        ) AS total_profit
+    FROM retail_orders
+    GROUP BY product_id
+)
+SELECT
+    product_id,
+    total_profit,
+    RANK() OVER (ORDER BY total_profit DESC) AS profit_rank
+FROM product_profit
+ORDER BY profit_rank;
+
+-- Source:
+-- Kaggle Dataset — Retail Orders
