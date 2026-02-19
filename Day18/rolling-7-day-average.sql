26830589b55114a39bf532ffd4880e12acc6c636
Zainab <z.alkholaif@gmail.com>
2026-02-19 07:39:15 +0300

    Update rolling-7-day-average.sql

--- Day18/rolling-7-day-average.sql
+++ Day18/rolling-7-day-average.sql
@@ -1,1 +1,28 @@
+-- Day 18: Rolling 7-Day Average Revenue
 
+-- Question:
+-- What is the 7-day moving average of daily revenue?
+
+-- Solution
+WITH daily_revenue AS (
+    SELECT
+        order_date,
+        SUM(
+            list_price * quantity * (1 - discount / 100.0)
+        ) AS daily_revenue
+    FROM retail_orders
+    GROUP BY order_date
+)
+SELECT
+    order_date,
+    daily_revenue,
+    AVG(daily_revenue)
+        OVER (
+            ORDER BY order_date
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS cumulative_revenue
+FROM daily_revenue
+ORDER BY order_date;
+
+-- Source:
+-- Kaggle Dataset — Retail Orders
