dd17d510b7a755637ad88e8ef2c394da523a1ef7
Zainab <z.alkholaif@gmail.com>
2026-02-20 02:31:00 +0300

    Update day-over-day-growth.sql

--- Day19/day-over-day-growth.sql
+++ Day19/day-over-day-growth.sql
@@ -1,1 +1,28 @@
+-- Day 19: Day-Over-Day Revenue Growth
 
+-- Question:
+-- What is the percentage change in revenue compared to the previous day?
+
+-- Solution
+WITH daily_revenue AS (
+    SELECT
+        order_date,
+        SUM(
+            list_price * quantity * (1 - discount / 100.0)
+        ) AS daily_revenue
+    FROM retail_orders
+    GROUP BY order_date
+)
+SELECT
+    order_date,
+    daily_revenue,
+    AVG(daily_revenue)
+        OVER (
+            ORDER BY order_date
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS cumulative_revenue
+FROM daily_revenue
+ORDER BY order_date;
+
+-- Source:
+-- Kaggle Dataset — Retail Orders
