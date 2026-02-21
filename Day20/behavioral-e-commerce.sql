0013a997c5de17c8f0da327586f20a636fac5c85
Zainab <z.alkholaif@gmail.com>
2026-02-21 03:40:35 +0300

    Update behavioral-e-commerce.sql

--- Day20/behavioral-e-commerce.sql
+++ Day20/behavioral-e-commerce.sql
@@ -1,1 +1,30 @@
+-- Day 20: Kaggle Dataset Exploration — Behavioral E-Commerce Performance Summary
+-- Dataset: E-Commerce Behavior Data from Multi-Category Store
+
+-- Columns:
+
+-- event_time, event_type, product_id, category_id,
+-- category_code, brand, price, user_id, user_session
+
+--------------------------------------------------
+-- 1. Total Revenue
+--------------------------------------------------
+
+
+--------------------------------------------------
+-- 2. Revenue by Order Status
+--------------------------------------------------
+
+
+--------------------------------------------------
+-- 3. Top 5 Products by Revenue
+--------------------------------------------------
+
+
+--------------------------------------------------
+-- 4. Average Order Value (AOV)
+--------------------------------------------------
+
+
+--------------------------------------------------
 
