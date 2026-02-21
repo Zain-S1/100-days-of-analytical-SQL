1fd6a5547211ede5820d8a759ebe16168c853198
Zainab <z.alkholaif@gmail.com>
2026-02-21 04:00:09 +0300

    Update behavioral-e-commerce.sql

--- Day20/behavioral-e-commerce.sql
+++ Day20/behavioral-e-commerce.sql
@@ -31,9 +31,24 @@ FROM ecommerce_events
 WHERE event_type = 'purchase';
 
 --------------------------------------------------
--- 4. Average Order Value (AOV)
+-- 4. Conversion Rate (Session-Level)
 --------------------------------------------------
+WITH sessions AS (
+    SELECT DISTINCT user_session
+    FROM ecommerce_events
+),
+purchase_sessions AS (
+    SELECT DISTINCT user_session
+    FROM ecommerce_events
+    WHERE event_type = 'purchase'
+)
 
+SELECT
+    COUNT(DISTINCT p.user_session) * 1.0
+    / COUNT(DISTINCT s.user_session) AS session_conversion_rate
+FROM sessions s
+LEFT JOIN purchase_sessions p
+    ON s.user_session = p.user_session;
 
 --------------------------------------------------
 
