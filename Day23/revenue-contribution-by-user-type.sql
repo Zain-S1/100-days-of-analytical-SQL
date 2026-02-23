f631ca9bb02a68070dc1c3a125f9f57cfa89db38
Zainab <z.alkholaif@gmail.com>
2026-02-24 02:37:14 +0300

    Update revenue-contribution-by-user-type.sql

--- Day23/revenue-contribution-by-user-type.sql
+++ Day23/revenue-contribution-by-user-type.sql
@@ -5,25 +5,16 @@
 -- compared to one-time buyers?
 
 -- Solution
-WITH session_steps AS (
+WITH user_purchase_counts AS (
     SELECT
-        user_session,
-        MAX(CASE WHEN event_type = 'view' THEN 1 ELSE 0) AS viewed,
-        MAX(CASE WHEN event_type = 'cart' THEN 1 ELSE 0) AS added_to_cart,
-        MAX(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0) AS purchased
+        user_id,
+        COUNT(*) AS purchase_count
     FROM ecommerce_events
-    GROUP BY user_session
-)
+    WHERE event_type = 'purchase'
+    GROUP BY user_id
+),
 
-SELECT
-    COUNT(*) AS total_sessions,
-    SUM(viewed) AS view_sessions,
-    SUM(added_to_cart) AS cart_sessions,
-    SUM(purchased) AS purchase_sessions,
-    SUM(added_to_cart) * 1.0 / SUM(viewed) AS view_to_cart_rate,
-    SUM(purchased) * 1.0 / SUM(added_to_cart) AS cart_to_purchase_rate,
-    SUM(purchased) * 1.0 / COUNT(*) AS overall_conversion_rate
-FROM session_steps;
+
 
 -- Source:
 -- Kaggle Dataset — E-Commerce Behavior Data from Multi-Category Store
