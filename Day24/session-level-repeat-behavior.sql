f2d1bf71252ec788186171df30fe45ca72536a94
Zainab <z.alkholaif@gmail.com>
2026-02-25 01:21:35 +0300

    Update session-level-repeat-behavior.sql

--- Day24/session-level-repeat-behavior.sql
+++ Day24/session-level-repeat-behavior.sql
@@ -1,1 +1,56 @@
+-- Day 24: Session-Level Repeat Purchase Behavior
 
+-- Question:
+-- How many sessions include multiple purchases,
+-- and how much revenue do they generate?
+
+-- Solution
+WITH user_purchase_counts AS (
+    SELECT
+        user_id,
+        COUNT(*) AS purchase_count
+    FROM ecommerce_events
+    WHERE event_type = 'purchase'
+    GROUP BY user_id
+),
+
+user_type AS (
+    SELECT
+    user_id,
+        CASE
+            WHEN purchase_count > 1 THEN 'Repeat Buyer'
+            ELSE 'One-Time Buyer'
+        END AS buyer_type
+    FROM user_purchase_counts
+),
+
+purchase_revenue AS (
+    SELECT
+        user_id,
+        SUM(price) AS total_user_revenue
+    FROM ecommerce_events
+    WHERE event_type = 'purchase'
+    GROUP BY user_id
+)
+
+SELECT
+    ut.buyer_type,
+    SUM(pr.total_user_revenue) AS revenue,
+    SUM(pr.total_user_revenue) + 1.0
+    / ( SELECT SUM(price)
+        FROM ecommerce_events
+        WHERE event_type = 'purchase' ) AS revenue_share
+FROM user_type ut
+JOIN purchase_revenue pr
+    ON ut.user_id = pr.user_id
+GROUP BY ut.buyer_type;
+
+-- Source:
+-- Kaggle Dataset — E-Commerce Behavior Data from Multi-Category Store
+
+------------------
+-- Results:
+------------------
+
+-- * Revenue Share by One-Time Buyers: 25%
+-- * Revenue Share by Repeat Buyers: 75%
