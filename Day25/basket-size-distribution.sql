453bce70f3d68e91f8e17ed22dcb10a0cb2aa20e
Zainab <z.alkholaif@gmail.com>
2026-02-26 07:13:27 +0300

    Create basket-size-distribution.sql

---
+++ Day25/basket-size-distribution.sql
@@ -0,0 +1,22 @@
+-- Day 25: asket Size Distribution
+
+-- Question:
+-- How are purchasing sessions distributed by
+-- number of products bought?
+
+-- Solution
+WITH session_product_counts AS (
+    SELECT
+        user_session,
+        COUNT(*) AS products_in_session
+    FROM ecommerce_events
+    WHERE event_type = 'purchase'
+    GROUP BY user_session
+)
+
+SELECT
+    AVG(products_in_session) AS average_basket_size
+FROM session_product_counts;
+
+-- Source:
+-- Kaggle Dataset — E-Commerce Behavior Data from Multi-Category Store
