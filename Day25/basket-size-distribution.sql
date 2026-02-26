0adca2f72abd0e498e399924991b4d5f0e9cc35e
Zainab <z.alkholaif@gmail.com>
2026-02-26 07:16:23 +0300

    Update basket-size-distribution.sql

--- Day25/basket-size-distribution.sql
+++ Day25/basket-size-distribution.sql
@@ -5,18 +5,19 @@
 -- number of products bought?
 
 -- Solution
-WITH session_product_counts AS (
+SELECT
+    products_in_session,
+    COUNT(*) AS number_of_sessions
+FROM (
     SELECT
         user_session,
         COUNT(*) AS products_in_session
     FROM ecommerce_events
     WHERE event_type = 'purchase'
     GROUP BY user_session
-)
-
-SELECT
-    AVG(products_in_session) AS average_basket_size
-FROM session_product_counts;
+) t
+GROUP BY products_in_session
+ORDER BY products_in_session;
 
 -- Source:
 -- Kaggle Dataset — E-Commerce Behavior Data from Multi-Category Store
