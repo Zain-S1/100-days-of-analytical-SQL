3bb835eb8317a3c4e4173077f2ef9718395410e9
Zainab <z.alkholaif@gmail.com>
2026-03-10 00:40:32 +0300

    Update day36-average-customer-lifetime.sql

--- 04-saas-subscription-analysis/day36-average-customer-lifetime.sql
+++ 04-saas-subscription-analysis/day36-average-customer-lifetime.sql
@@ -1,14 +1,20 @@
--- Day 36: Customer Lifetime
+-- Day 36: Average Customer Lifetime
 
 -- Question:
 -- How much revenue does a customer generate before they churn?
 
 -- Solution
+WITH customer_lifetime AS (
+    SELECT
+        customer_id,
+        COUNT(DISTINCT month) AS months_active
+    FROM subscriptions
+    GROUP BY customer_id
+)
+
 SELECT
-    customer_id,
-    COUNT(DISTINCT month) AS months_active
-FROM subscriptions
-GROUP BY customer_id;
+    AVG(months_active) AS average_customer_lifetime_months
+FROM customer_lifetime;
 
 -- Source:
 -- Kaggle Dataset — SaaS Business Metrics: Customers, Plans & Revenue
